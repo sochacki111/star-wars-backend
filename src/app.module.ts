72614b06733230aa7419493bdab1fa0a5f95c893
Michal Sochacki <michalsochacki111@gmail.com>
2024-07-01 12:25:16 +0200

    feat: install nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { CharactersModule } from './characters/characters.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', `.env.${process.env.NODE_ENV || 'development'}`],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
--- src/characters/characters.module.ts
+++ src/characters/characters.module.ts
@@ -8,7 +8,6 @@ import { CharactersRepositorySymbol } from './repositories/characters.repository
   providers: [
     CharactersService,
     CharactersResolver,
-    CharactersRepository,
     {
       provide: CharactersRepositorySymbol,
       useClass: CharactersRepository,
