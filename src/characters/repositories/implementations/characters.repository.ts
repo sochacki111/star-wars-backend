36c2984edfaeef6d7094412673ff4047cf4490a1
Michal Sochacki <michalsochacki111@gmail.com>
2024-07-03 11:21:08 +0200

    feat(characters.repository.ts): wrap db queries with transaction

--- src/characters/repositories/implementations/characters.repository.ts
+++ src/characters/repositories/implementations/characters.repository.ts
@@ -55,39 +55,41 @@ export class CharactersRepository implements CharactersRepositoryInterface {
       where: { name },
     });
 
-    if (existingCharacter) {
-      throw new Error(`Character with name ${name} already exists.`);
-    }
-
-    const episodes = await this.prisma.episode.findMany({
-      where: { id: { in: episodeIds } },
-    });
-
-    if (episodes.length !== episodeIds.length) {
-      throw new Error(`One or more episode IDs are invalid.`);
-    }
-
-    if (planetId) {
-      const planet = await this.prisma.planet.findUnique({
-        where: { id: planetId },
-      });
-      if (!planet) {
-        throw new Error(`Planet with ID ${planetId} does not exist.`);
+    return this.prisma.$transaction(async (prisma) => {
+      if (existingCharacter) {
+        throw new Error(`Character with name ${name} already exists.`);
       }
-    }
 
-    const character = await this.prisma.character.create({
-      data: {
-        name,
-        planetId,
-        episodes: {
-          create: episodeIds.map((episodeId) => ({ episodeId })),
+      const episodes = await prisma.episode.findMany({
+        where: { id: { in: episodeIds } },
+      });
+
+      if (episodes.length !== episodeIds.length) {
+        throw new Error(`One or more episode IDs are invalid.`);
+      }
+
+      if (planetId) {
+        const planet = await prisma.planet.findUnique({
+          where: { id: planetId },
+        });
+        if (!planet) {
+          throw new Error(`Planet with ID ${planetId} does not exist.`);
+        }
+      }
+
+      const character = await prisma.character.create({
+        data: {
+          name,
+          planetId,
+          episodes: {
+            create: episodeIds.map((episodeId) => ({ episodeId })),
+          },
         },
-      },
-      include: { episodes: { include: { episode: true } }, planet: true },
-    });
+        include: { episodes: { include: { episode: true } }, planet: true },
+      });
 
-    return this.mapCharacter(character);
+      return this.mapCharacter(character);
+    });
   }
 
   private mapCharacter = (character): Character => ({
@@ -104,53 +106,55 @@ export class CharactersRepository implements CharactersRepositoryInterface {
   ): Promise<Character> {
     const { name, episodeIds, planetId } = data;
 
-    const existingCharacter = await this.prisma.character.findUnique({
-      where: { id },
-    });
-
-    if (!existingCharacter) {
-      throw new Error(`Character with ID ${id} does not exist.`);
-    }
-
-    if (episodeIds) {
-      const episodes = await this.prisma.episode.findMany({
-        where: { id: { in: episodeIds } },
+    return this.prisma.$transaction(async (prisma) => {
+      const existingCharacter = await prisma.character.findUnique({
+        where: { id },
       });
 
-      if (episodes.length !== episodeIds.length) {
-        throw new Error(`One or more episode IDs are invalid.`);
+      if (!existingCharacter) {
+        throw new Error(`Character with ID ${id} does not exist.`);
       }
-    }
 
-    if (planetId) {
-      const planet = await this.prisma.planet.findUnique({
-        where: { id: planetId },
+      if (episodeIds) {
+        const episodes = await prisma.episode.findMany({
+          where: { id: { in: episodeIds } },
+        });
+
+        if (episodes.length !== episodeIds.length) {
+          throw new Error(`One or more episode IDs are invalid.`);
+        }
+      }
+
+      if (planetId) {
+        const planet = await prisma.planet.findUnique({
+          where: { id: planetId },
+        });
+        if (!planet) {
+          throw new Error(`Planet with ID ${planetId} does not exist.`);
+        }
+      }
+
+      const character = await prisma.character.update({
+        where: { id },
+        data: {
+          name,
+          planetId,
+          episodes: episodeIds
+            ? {
+                set: episodeIds.map((episodeId) => ({
+                  characterId_episodeId: {
+                    characterId: id,
+                    episodeId: episodeId,
+                  },
+                })),
+              }
+            : undefined,
+        },
+        include: { episodes: { include: { episode: true } }, planet: true },
       });
-      if (!planet) {
-        throw new Error(`Planet with ID ${planetId} does not exist.`);
-      }
-    }
 
-    const character = await this.prisma.character.update({
-      where: { id },
-      data: {
-        name,
-        planetId,
-        episodes: episodeIds
-          ? {
-              set: episodeIds.map((episodeId) => ({
-                characterId_episodeId: {
-                  characterId: id,
-                  episodeId: episodeId,
-                },
-              })),
-            }
-          : undefined,
-      },
-      include: { episodes: { include: { episode: true } }, planet: true },
+      return this.mapCharacter(character);
     });
-
-    return this.mapCharacter(character);
   }
 
   async remove(id: number): Promise<Character> {
